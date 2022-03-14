d62ec20318d6546a948ad660e6c1821d885e6360
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-03-14 21:17:00 +0100

    create initial home configuration
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-21.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-21.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       lib = nixpkgs.lib;
     in
@@ -17,6 +22,21 @@
           modules = [
             ./hosts/mark-g15/default.nix
           ];
+        };
+      };
+
+      homeConfigurations = {
+        "mark@mark-g15" = home-manager.lib.homeManagerConfiguration rec {
+          system = "x86_64-linux";
+
+          username = "mark";
+          homeDirectory = "/home/mark";
+
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          configuration = { };
         };
       };
     };
