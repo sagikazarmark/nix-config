b046da73166df3b0b17d415d5b5915e350cd71b8
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-03-20 01:06:40 +0100

    install git by default
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- hosts/mark-g15/default.nix
+++ hosts/mark-g15/default.nix
@@ -130,11 +130,12 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  #   firefox
-  # ];
+  environment.systemPackages = with pkgs; [
+    git
+    #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #   wget
+    #   firefox
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
