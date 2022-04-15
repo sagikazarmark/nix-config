fd3470b74de2d7f0493c7a3dc8eb445ce4cb2750
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-04-15 02:16:55 +0200

    add ntfs support
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- hosts/common/base.nix
+++ hosts/common/base.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
 {
+  boot.supportedFilesystems = [ "ntfs" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
