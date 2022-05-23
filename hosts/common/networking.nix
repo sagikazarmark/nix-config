6bf00212a218fc96269f281dd6bcec48faf00020
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-05-23 12:41:57 +0200

    open firewall port
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- hosts/common/networking.nix
+++ hosts/common/networking.nix
@@ -13,7 +13,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8081 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
