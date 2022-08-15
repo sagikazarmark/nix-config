d0697708c43b270f903c631e2ecb4b3d31a3d77d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-08-15 13:24:18 +0200

    pin go version for hostctl
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
       systemOverlay = (
         final: prev: {
-          keyd = prev.callPackage ./pkgs/keyd/default.nix { };
+          keyd = prev.callPackage ./pkgs/keyd/default.nix {};
         }
       );
 
@@ -42,178 +42,183 @@
               }
             );
           };
-          lr-tech-rofi-themes = prev.callPackage ./pkgs/lr-tech-rofi-themes/default.nix { };
+          lr-tech-rofi-themes = prev.callPackage ./pkgs/lr-tech-rofi-themes/default.nix {};
+
+          # Waiting for https://github.com/guumaster/hostctl/pull/85 to fix dependency problems
+          hostctl = prev.hostctl.override {
+            buildGoModule = final.buildGo117Module;
+          };
         }
       );
     in
-    {
-      nixosConfigurations = {
-        mark-desktop = lib.nixosSystem {
-          system = "x86_64-linux";
-
-          specialArgs = { inherit inputs; };
-
-          modules = [
-            {
-              nixpkgs.overlays = [ systemOverlay nur.overlay ];
-            }
-
-            ./hosts/mark-desktop
-            ./users/mark/system
-
-            ./modules/nixos/keyd.nix
-          ];
-        };
-
-        mark-g15 = lib.nixosSystem {
-          system = "x86_64-linux";
-
-          specialArgs = { inherit inputs; };
-
-          modules = [
-            {
-              nixpkgs.overlays = [ systemOverlay nur.overlay ];
-            }
-
-            ./hosts/mark-g15
-            ./users/mark/system
-
-            ./modules/nixos/keyd.nix
-          ];
-        };
-      };
-
-      homeConfigurations = {
-        "mark@mark-desktop" = home-manager.lib.homeManagerConfiguration rec {
-          system = "x86_64-linux";
-
-          username = "mark";
-          homeDirectory = "/home/mark";
-
-          pkgs = import nixpkgs {
-            inherit system;
-
-            config.allowUnfree = true;
-
-            overlays = [
-              linuxHomeOverlay
-            ];
-          };
-
-          extraModules = [
-            ./modules/home-manager
-            inputs.nix-colors.homeManagerModule
-          ];
-
-          extraSpecialArgs = { inherit inputs; };
-
-          configuration = {
-            imports = [
-              ./home.nix
-              ./users/mark/home/nix-colors.nix
-              ./users/mark/home/linux.nix
-              ./users/mark/home/dev.nix
-              ./users/mark/home/programs/git.nix
-              ./users/mark/home/programs/kitty
-              ./users/mark/home/programs/neomutt.nix
-            ];
-          };
-        };
-
-        "mark@mark-g15" = home-manager.lib.homeManagerConfiguration rec {
-          system = "x86_64-linux";
-
-          username = "mark";
-          homeDirectory = "/home/mark";
-
-          pkgs = import nixpkgs {
-            inherit system;
-
-            config.allowUnfree = true;
-
-            overlays = [
-              linuxHomeOverlay
-
-              (
-                final: prev: {
-                  libsForQt5 = prev.libsForQt5 // {
-                    qtstyleplugin-kvantum = prev.libsForQt5.qtstyleplugin-kvantum.overrideAttrs (
-                      o: rec {
-                        patches = [ ./pkgs/kvantum/kvantum.patch ];
-                        patchFlags = [ "-p2" ];
-                        cmakeFlags = [ "-DCMAKE_INSTALL_PREFIX=$(out)" ];
-                        makeFlags = [ "PREFIX=$(out)" ];
-                      }
-                    );
-                  };
-                }
-              )
-            ];
-          };
-
-          extraModules = [
-            ./modules/home-manager
-            inputs.nix-colors.homeManagerModule
-          ];
-
-          extraSpecialArgs = { inherit inputs; };
-
-          configuration = {
-            imports = [
-              ./home.nix
-              ./users/mark/home/nix-colors.nix
-              ./users/mark/home/gtk.nix
-              ./users/mark/home/linux.nix
-              ./users/mark/home/sway.nix
-              ./users/mark/home/dev.nix
-              ./users/mark/home/programs/git.nix
-              ./users/mark/home/programs/kitty
-              ./users/mark/home/programs/neomutt.nix
-            ];
-          };
-        };
-
-        "marksk@MARKSK-M-J1W8" = home-manager.lib.homeManagerConfiguration rec {
-          system = "x86_64-darwin";
-
-          username = "marksk";
-          homeDirectory = "/Users/marksk";
-
-          pkgs = import nixpkgsUnstable {
-            inherit system;
-
-            overlays = [ ];
-          };
-
-          extraModules = [
-            ./modules/home-manager
-            inputs.nix-colors.homeManagerModule
-          ];
-
-          extraSpecialArgs = { inherit inputs; };
-
-          configuration = {
-            imports = [
-              ./home.nix
-              ./home.darwin.nix
-              ./users/mark/home/nix-colors.nix
-              ./users/mark/home/dev.nix
-              ./users/mark/home/programs/git.nix
-              ./users/mark/home/programs/kitty
-              ./users/mark/home/programs/neomutt.nix
-            ];
-          };
-        };
-      };
-    } // inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
       {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ home-manager git ];
+        nixosConfigurations = {
+          mark-desktop = lib.nixosSystem {
+            system = "x86_64-linux";
+
+            specialArgs = { inherit inputs; };
+
+            modules = [
+              {
+                nixpkgs.overlays = [ systemOverlay nur.overlay ];
+              }
+
+              ./hosts/mark-desktop
+              ./users/mark/system
+
+              ./modules/nixos/keyd.nix
+            ];
+          };
+
+          mark-g15 = lib.nixosSystem {
+            system = "x86_64-linux";
+
+            specialArgs = { inherit inputs; };
+
+            modules = [
+              {
+                nixpkgs.overlays = [ systemOverlay nur.overlay ];
+              }
+
+              ./hosts/mark-g15
+              ./users/mark/system
+
+              ./modules/nixos/keyd.nix
+            ];
+          };
         };
-      }
-    );
+
+        homeConfigurations = {
+          "mark@mark-desktop" = home-manager.lib.homeManagerConfiguration rec {
+            system = "x86_64-linux";
+
+            username = "mark";
+            homeDirectory = "/home/mark";
+
+            pkgs = import nixpkgs {
+              inherit system;
+
+              config.allowUnfree = true;
+
+              overlays = [
+                linuxHomeOverlay
+              ];
+            };
+
+            extraModules = [
+              ./modules/home-manager
+              inputs.nix-colors.homeManagerModule
+            ];
+
+            extraSpecialArgs = { inherit inputs; };
+
+            configuration = {
+              imports = [
+                ./home.nix
+                ./users/mark/home/nix-colors.nix
+                ./users/mark/home/linux.nix
+                ./users/mark/home/dev.nix
+                ./users/mark/home/programs/git.nix
+                ./users/mark/home/programs/kitty
+                ./users/mark/home/programs/neomutt.nix
+              ];
+            };
+          };
+
+          "mark@mark-g15" = home-manager.lib.homeManagerConfiguration rec {
+            system = "x86_64-linux";
+
+            username = "mark";
+            homeDirectory = "/home/mark";
+
+            pkgs = import nixpkgs {
+              inherit system;
+
+              config.allowUnfree = true;
+
+              overlays = [
+                linuxHomeOverlay
+
+                (
+                  final: prev: {
+                    libsForQt5 = prev.libsForQt5 // {
+                      qtstyleplugin-kvantum = prev.libsForQt5.qtstyleplugin-kvantum.overrideAttrs (
+                        o: rec {
+                          patches = [ ./pkgs/kvantum/kvantum.patch ];
+                          patchFlags = [ "-p2" ];
+                          cmakeFlags = [ "-DCMAKE_INSTALL_PREFIX=$(out)" ];
+                          makeFlags = [ "PREFIX=$(out)" ];
+                        }
+                      );
+                    };
+                  }
+                )
+              ];
+            };
+
+            extraModules = [
+              ./modules/home-manager
+              inputs.nix-colors.homeManagerModule
+            ];
+
+            extraSpecialArgs = { inherit inputs; };
+
+            configuration = {
+              imports = [
+                ./home.nix
+                ./users/mark/home/nix-colors.nix
+                ./users/mark/home/gtk.nix
+                ./users/mark/home/linux.nix
+                ./users/mark/home/sway.nix
+                ./users/mark/home/dev.nix
+                ./users/mark/home/programs/git.nix
+                ./users/mark/home/programs/kitty
+                ./users/mark/home/programs/neomutt.nix
+              ];
+            };
+          };
+
+          "marksk@MARKSK-M-J1W8" = home-manager.lib.homeManagerConfiguration rec {
+            system = "x86_64-darwin";
+
+            username = "marksk";
+            homeDirectory = "/Users/marksk";
+
+            pkgs = import nixpkgsUnstable {
+              inherit system;
+
+              overlays = [];
+            };
+
+            extraModules = [
+              ./modules/home-manager
+              inputs.nix-colors.homeManagerModule
+            ];
+
+            extraSpecialArgs = { inherit inputs; };
+
+            configuration = {
+              imports = [
+                ./home.nix
+                ./home.darwin.nix
+                ./users/mark/home/nix-colors.nix
+                ./users/mark/home/dev.nix
+                ./users/mark/home/programs/git.nix
+                ./users/mark/home/programs/kitty
+                ./users/mark/home/programs/neomutt.nix
+              ];
+            };
+          };
+        };
+      } // inputs.flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+            {
+              devShell = pkgs.mkShell {
+                buildInputs = with pkgs; [ home-manager git ];
+              };
+            }
+      );
 }
