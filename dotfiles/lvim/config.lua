0e0775625e27db4ef34a80e7c0f8896ab7cc1abd
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-09-07 18:57:58 +0200

    add back lunarvim tab navigation shortcuts
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- dotfiles/lvim/config.lua
+++ dotfiles/lvim/config.lua
@@ -10,6 +10,11 @@ lvim.colorscheme = "tokyonight"
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+-- https://github.com/LunarVim/LunarVim/pull/2874
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = ""
 -- edit a default keymapping
@@ -19,30 +24,30 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-  },
+    -- for input mode
+    i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+    },
+    -- for normal mode
+    n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+    },
 }
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+    name = "+Trouble",
+    r = { "<cmd>Trouble lsp_references<cr>", "References" },
+    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+    d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+    w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
 }
 
 -- TODO: User Config for predefined plugins
@@ -56,23 +61,23 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "css",
-  "dockerfile",
-  "go",
-  "graphql",
-  "hcl",
-  "gomod",
-  "javascript",
-  "json",
-  "lua",
-  "php",
-  "rust",
-  "toml",
-  "typescript",
-  "yaml",
-  "vim",
-  "nix",
+    "bash",
+    "css",
+    "dockerfile",
+    "go",
+    "graphql",
+    "hcl",
+    "gomod",
+    "javascript",
+    "json",
+    "lua",
+    "php",
+    "rust",
+    "toml",
+    "typescript",
+    "yaml",
+    "vim",
+    "nix",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -148,32 +153,32 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
 require("lvim.lsp.manager").setup("gopls", {
-  cmd = { "gopls" }
+    cmd = { "gopls" }
 })
 
 require("lvim.lsp.manager").setup("rnix", {
-  cmd = { "rnix-lsp" }
+    cmd = { "rnix-lsp" }
 })
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  {
-    name = "clang_format",
-    filetypes = { "proto" },
-    extra_args = { "--style", "{BasedOnStyle: Google, IndentWidth: 4}" },
-  },
+    {
+        name = "clang_format",
+        filetypes = { "proto" },
+        extra_args = { "--style", "{BasedOnStyle: Google, IndentWidth: 4}" },
+    },
 
-  --   { exe = "black", filetypes = { "python" } },
-  --   { exe = "isort", filetypes = { "python" } },
-  --   {
-  --     exe = "prettier",
-  --     ---@usage arguments to pass to the formatter
-  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --     args = { "--print-with", "100" },
-  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --     filetypes = { "typescript", "typescriptreact" },
-  --   },
+    --   { exe = "black", filetypes = { "python" } },
+    --   { exe = "isort", filetypes = { "python" } },
+    --   {
+    --     exe = "prettier",
+    --     ---@usage arguments to pass to the formatter
+    --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    --     args = { "--print-with", "100" },
+    --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    --     filetypes = { "typescript", "typescriptreact" },
+    --   },
 }
 
 -- -- set additional linters
@@ -195,121 +200,121 @@ formatters.setup {
 
 -- Additional Plugins
 lvim.plugins = {
-  { "lunarvim/colorschemes" },
-  { "folke/tokyonight.nvim" },
-  {
-    "ray-x/lsp_signature.nvim",
-    config = function() require "lsp_signature".on_attach() end,
-    event = "InsertEnter"
-  },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-  },
-  {
-    "ray-x/go.nvim",
-    config = function() require('go').setup() end,
-  },
-  -- {"fatih/vim-go"},
-  -- {
-  --   "tpope/vim-surround",
-  --   keys = {"c", "d", "y"}
-  -- },
-  -- {
-  --   "blackCauldron7/surround.nvim",
-  --   config = function()
-  --     require"surround".setup {mappings_style = "surround"}
-  --   end
-  -- },
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup({ "*" }, {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      })
-    end,
-  },
-  {
-    "wakatime/vim-wakatime"
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    ft = "markdown",
-    config = function()
-      vim.g.mkdp_auto_start = 1
-    end,
-  },
-  {
-    "vim-test/vim-test"
-  },
-  {
-    "editorconfig/editorconfig-vim",
-  },
-  {
-    "jbyuki/instant.nvim",
-    config = function()
-      vim.g.instant_username = "mark"
-    end
-  },
-  {
-    "weilbith/nvim-code-action-menu"
-  },
-  {
-    "kosayoda/nvim-lightbulb"
-  },
-  {
-    "tridactyl/vim-tridactyl"
-  },
-  {
-    "aklt/plantuml-syntax"
-  },
-  {
-    "tyru/open-browser.vim"
-  },
-  {
-    "weirongxu/plantuml-previewer.vim"
-  },
-  {
-    "jjo/vim-cue"
-  }
+    { "lunarvim/colorschemes" },
+    { "folke/tokyonight.nvim" },
+    {
+        "ray-x/lsp_signature.nvim",
+        config = function() require "lsp_signature".on_attach() end,
+        event = "InsertEnter"
+    },
+    {
+        "folke/trouble.nvim",
+        cmd = "TroubleToggle",
+    },
+    {
+        "simrat39/symbols-outline.nvim",
+        cmd = "SymbolsOutline",
+    },
+    {
+        "ray-x/go.nvim",
+        config = function() require('go').setup() end,
+    },
+    -- {"fatih/vim-go"},
+    -- {
+    --   "tpope/vim-surround",
+    --   keys = {"c", "d", "y"}
+    -- },
+    -- {
+    --   "blackCauldron7/surround.nvim",
+    --   config = function()
+    --     require"surround".setup {mappings_style = "surround"}
+    --   end
+    -- },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "*" }, {
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+        end,
+    },
+    {
+        "wakatime/vim-wakatime"
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        ft = "markdown",
+        config = function()
+            vim.g.mkdp_auto_start = 1
+        end,
+    },
+    {
+        "vim-test/vim-test"
+    },
+    {
+        "editorconfig/editorconfig-vim",
+    },
+    {
+        "jbyuki/instant.nvim",
+        config = function()
+            vim.g.instant_username = "mark"
+        end
+    },
+    {
+        "weilbith/nvim-code-action-menu"
+    },
+    {
+        "kosayoda/nvim-lightbulb"
+    },
+    {
+        "tridactyl/vim-tridactyl"
+    },
+    {
+        "aklt/plantuml-syntax"
+    },
+    {
+        "tyru/open-browser.vim"
+    },
+    {
+        "weirongxu/plantuml-previewer.vim"
+    },
+    {
+        "jjo/vim-cue"
+    }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands = {
-  {
-    "BufWinEnter",
     {
-      pattern = { "*.php" },
-      command = "setlocal ts=4 sw=4",
-    }
-  },
-  {
-    "BufWinEnter",
+        "BufWinEnter",
+        {
+            pattern = { "*.php" },
+            command = "setlocal ts=4 sw=4",
+        }
+    },
     {
-      pattern = { "*.yaml.dist" },
-      command = "setlocal filetype=yaml",
-    }
-  },
-  {
-    "BufWinEnter",
+        "BufWinEnter",
+        {
+            pattern = { "*.yaml.dist" },
+            command = "setlocal filetype=yaml",
+        }
+    },
     {
-      pattern = { "Makefile" },
-      command = "setlocal noexpandtab",
-    }
-  },
-  -- { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
-  -- { "BufWinEnter", "*.php", "setlocal ts=4 sw=4" },
-  -- { "BufWinEnter", "*.yaml.dist", "setlocal filetype=yaml" },
-  -- { "BufWinEnter", "Makefile", "setlocal noexpandtab" },
+        "BufWinEnter",
+        {
+            pattern = { "Makefile" },
+            command = "setlocal noexpandtab",
+        }
+    },
+    -- { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+    -- { "BufWinEnter", "*.php", "setlocal ts=4 sw=4" },
+    -- { "BufWinEnter", "*.yaml.dist", "setlocal filetype=yaml" },
+    -- { "BufWinEnter", "Makefile", "setlocal noexpandtab" },
 }
