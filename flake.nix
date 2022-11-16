778c946e5ea2a7a354dd88018307ad5e73fdef69
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-11-16 15:26:52 +0100

    darwin config
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -107,8 +107,22 @@
               services.nix-daemon.enableSocketListener = true;
             }
             {
+              # Set keyboard speed
               system.defaults.NSGlobalDomain.KeyRepeat = 1;
               system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
+
+              # Enable dark mode
+              system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark";
+
+              # Automatically hide and show the Dock
+              system.defaults.dock.autohide = true;
+
+              # Smart settings
+              # https://derflounder.wordpress.com/2014/02/01/disabling-smart-quotes-in-mavericks/
+              system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+              system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+              system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+              system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
             }
             {
               services.skhd.enable = true;
