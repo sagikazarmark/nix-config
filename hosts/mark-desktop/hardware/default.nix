96d670752e071643e4c2e40e65a8eef4d30326ba
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-04-02 23:10:17 +0200

    feat: add focusrite scarlet solo driver
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- hosts/mark-desktop/hardware/default.nix
+++ hosts/mark-desktop/hardware/default.nix
@@ -12,6 +12,12 @@
     ../../common/hardware/logitech.nix
   ];
 
+  # Focusrite Scarlet Solo driver
+  # https://github.com/Focusrite-Scarlett-on-Linux/sound-usb-kernel-module#enabling-new-functionality-at-load-time
+  boot.extraModprobeConfig = ''
+    options snd_usb_audio vid=0x1235 pid=0x8211 device_setup=1
+  '';
+
   networking.interfaces.enp12s0f3u4.useDHCP = true;
   networking.interfaces.enp7s0.useDHCP = true;
 
