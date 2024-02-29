1579cc233ed94289111ae4f751e8d4a573eecc95
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-02-29 15:12:09 +0100

    chore: fix fzf
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- modules/home-manager/modules/nix-colors/programs/fzf.nix
+++ modules/home-manager/modules/nix-colors/programs/fzf.nix
@@ -19,9 +19,9 @@ in
   config = mkIf cfg.nix-colors.enable {
     programs.fzf.defaultOptions = [
       # https://github.com/fnune/base16-fzf
-      "--color=bg+:#${ colorscheme.palette.base01 },bg:#${ colorscheme.palette.base00 },spinner:#${ colorscheme.palette.base0C },hl:#${ colorscheme.palette.base0D }"
-      "--color=fg:#${ colorscheme.palette.base04 },header:#${ colorscheme.palette.base0D },info:#${ colorscheme.palette.base0A },pointer:#${ colorscheme.palette.base0C }"
-      "--color=marker:#${ colorscheme.palette.base0C },fg+:#${ colorscheme.palette.base06 },prompt:#${ colorscheme.palette.base0A },hl+:#${ colorscheme.palette.base0D }"
+      "--color=bg+:#${colorscheme.palette.base01},bg:#${colorscheme.palette.base00},spinner:#${colorscheme.palette.base0C},hl:#${colorscheme.palette.base0D}"
+      "--color=fg:#${colorscheme.palette.base04},header:#${colorscheme.palette.base0D},info:#${colorscheme.palette.base0A},pointer:#${colorscheme.palette.base0C}"
+      "--color=marker:#${colorscheme.palette.base0C},fg+:#${colorscheme.palette.base06},prompt:#${colorscheme.palette.base0A},hl+:#${colorscheme.palette.base0D}"
     ];
   };
 }
