742becb31c3fffabb035272de4a1381a75e144da
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-11-01 23:03:53 +0100

    add keyboard layouts
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- hosts/common/darwin/keyboard/default.nix
+++ hosts/common/darwin/keyboard/default.nix
@@ -3,7 +3,7 @@
 {
   # TODO: use linking instead of copying?
   system.activationScripts.extraActivation.text = ''
-    echo "copying keyboard layouts" >&2
+    echo "setting up keyboard layouts..." >&2
 
     $DRY_RUN_CMD cp -r $VERBOSE_ARG ${./Hungarian_Win.keylayout} "/Library/Keyboard Layouts/Hungarian_Win.keylayout"
     $DRY_RUN_CMD cp -r $VERBOSE_ARG ${./Hungarian_Win_v2.keylayout} "/Library/Keyboard Layouts/Hungarian_Win_v2.keylayout"
